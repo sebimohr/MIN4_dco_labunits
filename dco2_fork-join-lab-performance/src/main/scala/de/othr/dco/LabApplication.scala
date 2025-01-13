ad38ed713ec17e0d17e31276191ca841ba4d6177
Sebastian Mohr <sebastianmohr96@gmail.com>
2025-01-13 15:51:20 +0100

    Add simple recursive approach for dco2

--- dco2_fork-join-lab-performance/src/main/scala/de/othr/dco/LabApplication.scala
+++ dco2_fork-join-lab-performance/src/main/scala/de/othr/dco/LabApplication.scala
@@ -4,16 +4,45 @@ import com.github.kiprobinson.bigfraction.BigFraction
 
 import java.math.BigInteger
 import java.util.Random
+import java.util.concurrent.{ForkJoinPool, RecursiveAction}
 
+object Constants {
+  val THRESHOLD = 12
+}
 
 object LabApplication extends App {
+  private val MaxNumbers = 10000
 
-  val MaxNumbers = 10000
-
-  val numbers = LazyList.continually( ( new BigInteger(150000, new Random()), new BigInteger(15000, new Random()) ) )
+  private val numbers = LazyList.continually( ( new BigInteger(150000, new Random()), new BigInteger(15000, new Random()) ) )
     .take(MaxNumbers)
     .toArray
 
-  // Have fun :-)
+  println("Numbers are ready.")
 
+  private val pool = new ForkJoinPool(Runtime.getRuntime.availableProcessors())
+  private val task = new BigFractionTask(numbers, 0, numbers.length - 1)
+
+  println("Invoking task.")
+
+  pool.invoke(task)
+}
+
+class BigFractionTask(array: Array[(BigInteger, BigInteger)], start: Int, end: Int) extends RecursiveAction {
+  override def compute(): Unit = {
+    if (end - start < Constants.THRESHOLD) {
+      (start to end).foreach { i => {
+        BigFraction.valueOf(array(i)._1, array(i)._2)
+      }}
+      println(s"Computed ${end - start} numbers.")
+    } else {
+      println(s"Creating new tasks, numbers remaining: ${end - start}")
+      val mid = start + (end - start) / 2
+
+      val leftTask = new BigFractionTask(array, start, mid).fork()
+      val rightTask = new BigFractionTask(array, mid + 1, end).fork()
+
+      leftTask.join()
+      rightTask.join()
+    }
+  }
 }
